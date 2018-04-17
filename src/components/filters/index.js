684dda48a2a1ec1d9b1015c9809b816bf73350d3
Senthil Kumar <ssk381991@gmail.com>
2018-04-17 11:06:19 +0530

    Working Code!

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 
-
-
 class SearchForm extends Component {
-
     render() { 
     return (
         <div className="container">
             <input type="text" onKeyUp={event => this.props.filterBy(event.target.value)} placeholder="Filter by name" />
-
         </div>
-    )
+        )
+    }
 }
-}
-
 export default SearchForm;
